1eaf1d54d7f71932387551e4c462d7f9c79a0ce4
Shivam Gupta <shivamguptasunita111@gmail.com>
2024-11-27 15:30:09 +0530

    react-food

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        {/* <Route path="/Section2 " element={<Section2 />} />
+          <Route path="/menu" element={<Section3 />} />
+          <Route path="/shop" element={<Section4 />} />
+          <Route path="/blog" element={<Section5 />} />
+          <Route path="/contact" element={<Section6 />} />
+          <Route path="/footer" element={<Section7 />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
